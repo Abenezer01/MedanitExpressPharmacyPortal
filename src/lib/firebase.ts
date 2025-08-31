87a322bab516311557a9fac1ee0154b998c34bce
Abenezer Seyoum <enkusilasie7@gmail.com>
2025-08-31 11:16:40 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For more information on this, see: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  apiKey: "AIzaSyAiEoTSqIuoG7JKOqwvJJZdzbYFtm15heE",
+  authDomain: "meditelegram-84fnm.firebaseapp.com",
+  projectId: "meditelegram-84fnm",
+  storageBucket: "meditelegram-84fnm.appspot.com",
+  messagingSenderId: "533075998920",
+  appId: "1:533075998920:web:957a2f57f8075daccdcdfd"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
