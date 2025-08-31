a62446ee676d33e985b477781b4a846db9508363
Abenezer Seyoum <enkusilasie7@gmail.com>
2025-08-31 11:26:47 +0000

    can you fix the layout on there is a lot space above the dashboard

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -69,7 +69,7 @@ function AppLayout({ children }: { children: ReactNode }) {
             </div>
             <UserNav />
           </header>
-          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <main className="flex flex-1 flex-col p-4 lg:p-6">
             {children}
           </main>
         </div>
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,11 +8,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAiEoTSqIuoG7JKOqwvJJZdzbYFtm15heE",
   authDomain: "meditelegram-84fnm.firebaseapp.com",
   projectId: "meditelegram-84fnm",
-  storageBucket: "meditelegram-84fnm.appspot.com",
+  storageBucket: "meditelegram-84fnm.firebasestorage.app",
   messagingSenderId: "533075998920",
   appId: "1:533075998920:web:957a2f57f8075daccdcdfd"
 };
-
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
